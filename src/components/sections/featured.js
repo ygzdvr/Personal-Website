03951a1ac89530c54f99ed8bcac9a263f0d18fb2
Yagiz Devre <124176744+ygzdvr@users.noreply.github.com>
2024-01-18 16:15:30 +0300

    we are so back

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -348,7 +348,7 @@ const Featured = () => {
   return (
     <section id="projects">
       <h2 className="numbered-heading" ref={revealTitle}>
-        Some Things I’ve Built
+        Things I’ve Built
       </h2>
 
       <StyledProjectsGrid>

b89a081f3fa00d93bf89ef629a50d82ed621505d
Yagiz Devre <124176744+ygzdvr@users.noreply.github.com>
2024-01-18 15:42:40 +0300

    we are so back

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Contact } from '@components';
+import { Layout, Hero, About, Jobs, Publications, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -13,6 +13,7 @@ const IndexPage = ({ location }) => (
       <Hero />
       <About />
       <Jobs />
+      <Publications />
       <Contact />
     </StyledMainContainer>
   </Layout>
